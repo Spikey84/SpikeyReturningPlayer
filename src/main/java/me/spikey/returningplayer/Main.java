01e31582c7f37a5defdd6bdcdf6eda06183ab8e6
main <creativename1111@gmail.com>
2022-01-05 20:48:55 -0500

    redoing things

--- src/main/java/me/spikey/returningplayer/Main.java
+++ src/main/java/me/spikey/returningplayer/Main.java
@@ -1,20 +1,43 @@
 package me.spikey.returningplayer;
 
+import com.google.common.collect.Lists;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Main extends JavaPlugin implements Listener {
+
+    private ArrayList<UUID> bypassFull;
+
     @Override
     public void onEnable() {
+        this.bypassFull = Lists.newArrayList();
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
-    public void preLogin(AsyncPlayerPreLoginEvent event) {
-        if (!event.getLoginResult().equals(AsyncPlayerPreLoginEvent.Result.KICK_FULL)) return;
-        if (!Bukkit.getOfflinePlayer(event.getUniqueId()).hasPlayedBefore()) event.setLoginResult(AsyncPlayerPreLoginEvent.Result.ALLOWED);
+    @EventHandler(priority = EventPriority.HIGH)
+    public void prelogin(AsyncPlayerPreLoginEvent event) {
+        if (!Bukkit.getOfflinePlayer(event.getUniqueId()).hasPlayedBefore() && event.getLoginResult().equals(AsyncPlayerPreLoginEvent.Result.ALLOWED)) bypassFull.add(event.getUniqueId());
     }
+
+
+    @EventHandler
+    public void login(PlayerLoginEvent event) {
+
+        if (event.getResult().equals(PlayerLoginEvent.Result.KICK_FULL) && bypassFull.contains(event.getPlayer().getUniqueId())) {
+            event.allow();
+            event.setResult(PlayerLoginEvent.Result.ALLOWED);
+        }
+        bypassFull.remove(event.getPlayer().getUniqueId());
+    }
+
+
 }
